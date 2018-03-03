7f8c27ef360eed74ee8d15c47817d3b252283027
yuanyuan9826 <15135094322@163.com>
2018-03-03 18:53:19 +0800

    list2

--- model/GoodsModel.js
+++ model/GoodsModel.js
@@ -1,30 +1,20 @@
-var mongoose = require("mongoose");
+var mongoose = require( "mongoose" );
 var Schema = mongoose.Schema;
-
-// 创建文档的定义
+ 
+/*创建文档定义*/
 var Goods = new Schema({
-    goods_name  : String,             //商品名称
-    goods_num  : String,				//商品货号
-    goods_class : String,				//商品分类	
-    goods_extend_class : String,			//扩展分类
-    goods_brand : String,					//商品品牌
-    price_me       : String,					//本店价格
-    price_member_zc      : String,				//注册用户价格
-    price_member_dx      : String,				//代销用户价格
-    price_member_vip      : String,				//vip价格
-    price_dis_num			: String,			//优惠数量
-    price_dis			: String,				//优惠价格
-    price_bazaar			: String,			//市场价格
-    virtual_sales			: String,			//虚拟销量
-    
-    
-    
-    img       : String,
-    create_date : { type: Date, default: Date.now }
+    goods_name  : String,//商品名称
+    goods_id    : String,//货号
+    goods_price : String,//本店价格
+    count       : Number,//库存
+    goods_num   : Number,//虚拟销量
+    pic         : String,//商品图片
+    create_Date : { type: Date, default: Date.now }
 });
 
-// 创建model对象，与数据库中的文档（表）映射
+/*创建model对象，与数据库中的文档（表）映射*/
 var GoodsModel = mongoose.model('goods', Goods);
-// commonJS规范(暴露接口)
+
+/*使用commonJS规范暴露接口*/
 module.exports = GoodsModel;
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var UserModel = require( "../model/UserModel" );
 var GoodsModel = require( "../model/GoodsModel" );
-/*var multiparty = require( "multiparty" );*/
+var multiparty = require( "multiparty" );
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
@@ -11,13 +11,26 @@ router.get('/index', function(req, res, next) {
 router.get('/add', function(req, res, next) {
   res.render('add', { title: '商品添加' });
 });
+/*商品列表页*/
 router.get('/list', function(req, res, next) {
-  res.render('list', { title: 'Express' });
+	GoodsModel.find({}, function(err, docs) {
+		res.render('list', {list: docs});
+	})
 });
 router.get('/', function(req, res, next) {
-  res.render('login', { title: '登陆' });
+	res.render('login', { title: '登陆' });
+  // 检查用户是否登录
+if(req.session && req.session.username != null) {
+	res.render('login', { title: '登陆' });
+} else {
+	// 重定向
+	res.redirect('/index/login');
+}
 });
-/*后台主页面主要内容区*/
+
+
+	
+/*后台主页面内容区*/
 router.get('/content', function(req, res, next) {
   res.render('content', {});
 });
@@ -41,6 +54,68 @@ router.post('/api/login', function(req, res){
 			res.send(result);
 		}
 	})
+});
+
+/* 添加商品 */
+router.post("/add_goods", function(req, res){
+	var Form = new multiparty.Form({
+		uploadDir: "./public/goods_img"
+	})
+	Form.parse(req, function(err, body, files){
+		var goods_name = body.goods_name[0];
+		var goods_id = body.goods_id[0];
+		var goods_price = body.goods_price[0];
+		var count = body.count[0];
+		var goods_num = body.goods_num[0];
+		var picName = files.pic[0].path;
+		picName = picName.substr(picName.lastIndexOf("\\") + 1);
+//		console.log(goods_name,goods_id,goods_price,count,goods_num,picName);
+
+		var gm = new GoodsModel();
+		gm.goods_name = goods_name;
+		gm.goods_id = goods_id;
+		gm.goods_price = goods_price;
+		gm.count = count;
+		gm.goods_num = goods_num;
+		gm.pic = picName;
+		gm.save(function(err){
+			if(!err) {
+				res.send("商品保存成功");
+			} else {
+				res.send("商品保存失败");
+			}
+		})
+	})
+});
+
+/* 删除商品行 */
+router.post('/api/list', function(req, res, next) {
+	var goods_name = req.body.goods_name;
+	var goods_id = req.body.goods_id;
+	console.log(req.body);//{ goods_name: 'goods02', goods_id: '002' }
+	var del = { goods_name: goods_name, goods_id: goods_id };
+	GoodsModel.remove(del,function(err,result){
+	  if( !err ){
+	    console.log( "删除成功" );
+	  }else{
+	  	console.log( "删除失败" );
+	  }
+	  console.log(result);//{ n: 1, ok: 1 }
+	  res.send( result );
+	});
+	
+});
+/* 搜索--按商品名称进行模糊查询 */
+router.post('/search_goods', function(req, res, next){
+	var form = new multiparty.Form();
+	form.parse(req, function(err, body, files) {
+	  var goods_keywords = body.goods_keywords[0];
+	  GoodsModel.find({ goods_name: { $regex: goods_keywords } }, function(err, docs) {
+	  	res.render('list', {list: docs});
+	  })
+	});
 })
+
+
 
 module.exports = router;
